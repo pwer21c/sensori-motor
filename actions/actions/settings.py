c01e6d0da799284e88aca371c71ca06ce2ad0073
BANG sanghun <pwer21c@gmail.com>
2013-03-05 12:03:40 +0100

    setting.py

--- actions/actions/settings.py
+++ actions/actions/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'actions',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'znslq4kfkd',                  # Not used with sqlite3.
+        'PASSWORD': 'shbang',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
